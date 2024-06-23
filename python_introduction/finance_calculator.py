ca8a87d5c7ca85ccb4d17ddc844fe32d0f39fc05
Moses Wanjohi <wanjohimuturi.tech@gmail.com>
2024-06-23 03:58:24 -0700

    Created a Personal Finance Calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,10 @@
 monthly_income = input("Enter your monthly income: ")
 total_monthly_expenses = input( "Enter your total  monthly expenses: ")
-difference = float(monthly_income) - float(total_monthly_expenses)
-print("Your monthly savings are", difference)
+monthly_savings = float(monthly_income) - float(total_monthly_expenses)
+print("Your monthly savings are", monthly_savings)
 #Assume a simple annual interest rate of 5%.
 #Calculate the projected savings after one year, incorporating the interest.
 #Use the simplified formula for annual savings projection: (Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05))
-monthly_savings = difference
 annual_savings = monthly_savings * 12
 interest = annual_savings * 0.05
 projected_savings = annual_savings + interest
