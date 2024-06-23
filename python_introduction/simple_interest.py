125ce1c585fdbd61fd969308ef32fad24e8953b9
Moses Wanjohi <wanjohimuturi.tech@gmail.com>
2024-06-22 23:55:00 -0700

    added simple interst calculator and executed the script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000
+rate = 0.05
+time = 3
+#Calculate the simple interest using the formula provided and store the result in a variable named interest.
+interest = principal * rate * time #(I = P * R * T)
+print("The simple interest is:", interest)
+
