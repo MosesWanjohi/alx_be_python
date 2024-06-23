2639aa80a06c9841434f0129e3adb6c279e90004
Moses Wanjohi <wanjohimuturi.tech@gmail.com>
2024-06-23 05:07:40 -0700

    Created a Personal Financial Calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthly_income = input("Enter your monthly income: ")
-monthly_expenses = input( "Enter your total  monthly expenses: ")
-monthly_savings = float(monthly_income) - float(monthly_expenses)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input( "Enter your total  monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 print("Your monthly savings are", monthly_savings)
 #Assume a simple annual interest rate of 5%.
 #Calculate the projected savings after one year, incorporating the interest.
