781740a38596d36086a0e1fb5e07b26ea4b2d826
Moses Wanjohi <wanjohimuturi.tech@gmail.com>
2024-06-30 05:34:10 -0700

    Reminder app

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,12 +1,14 @@
 task = str(input("Enter your task: "))
 priority = str(input("Enter task's Priority (high/medium/low): "))
 time_bound = str(input("Is it time-bound? (yes/no): "))
-message = "that requires immediate attention today!"
+
+#message = "that requires immediate attention today!"
 
 match priority:
 
     case "high":
         if time_bound == "yes":
+            message = "that requires immediate attention today!"
             print(task,  "is a", priority, "priority task", message)
         else:
             print(task,  "is a", priority, "priority task.", "Consider completing it when you have free time")
@@ -24,4 +26,5 @@ match priority:
             print(task,  "is a", priority, "priority task", message)
         else:
             print(task,  "is a", priority, "priority task.", "Consider completing it when you have free time")
-print()
+    case _:
+        print("Invalid priority input. Please enter high, medium, or low.")
