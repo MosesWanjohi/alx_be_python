ad7feeedc9321005b2906ca3a1783e7dd6a89b26
Moses Wanjohi <wanjohimuturi.tech@gmail.com>
2024-06-30 04:12:05 -0700

    Modified reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
-task = str(input("Enter your task: "))
-priority = str(input("Enter task's priority (high, medium, low): "))
-time_bound = str(input("Is the task time bound? (yes, no): "))
+Task = str(input("Enter your task: "))
+Priority = str(input("Enter task's priority (high, medium, low): "))
+Time_Bound = str(input("Is the task time bound? (yes, no): "))
 message = "that requires immediate attention today!"
 
 match priority:
