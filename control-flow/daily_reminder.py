fd8db90581eba5fbb3ccb3bd6113eb87f8da2925
Moses Wanjohi <wanjohimuturi.tech@gmail.com>
2024-06-30 04:06:38 -0700

    Created a Personal Daily Reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,27 @@
+task = str(input("Enter your task: "))
+priority = str(input("Enter task's priority (high, medium, low): "))
+time_bound = str(input("Is the task time bound? (yes, no): "))
+message = "that requires immediate attention today!"
+
+match priority:
+
+    case "high":
+        if time_bound == "yes":
+            print(task,  "is a", priority, "priority task", message)
+        else:
+            print(task,  "is a", priority, "priority task.", "Consider completing it when you have free time")
+
+    case "medium":
+        if time_bound == "yes":
+            message = "that requires moderate attention today!"
+            print(task,  "is a", priority, "priority task", message)
+        else:
+            print(task,  "is a", priority, "priority task.", "Consider completing it when you have free time")
+
+    case "low":
+        if time_bound == "yes":
+            message = "that requires less attention today!"
+            print(task,  "is a", priority, "priority task", message)
+        else:
+            print(task,  "is a", priority, "priority task.", "Consider completing it when you have free time")
+print()
